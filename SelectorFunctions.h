46c11f485e20abbaee213af5b0ccb6538cdce321
Matt Haidet <plusallofyou@gmail.com>
2014-09-18 14:59:05 -0400

    Added Selector Functions File

---
+++ SelectorFunctions.h
@@ -0,0 +1,26 @@
+
+//LCD selector Code
+
+
+int getSelectionNumber(){
+return NULL;
+}
+
+
+void AutonomouseSelector(){
+switch(getSelectionNumber()){
+
+case 0:
+break;
+
+
+case 1:
+break;
+
+
+case 2:
+break;
+
+
+}
+}
