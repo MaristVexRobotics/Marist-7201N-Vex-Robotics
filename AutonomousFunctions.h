6125f491d72490b11cc96898d5b6a5cc6175559b
Nico99 <NickOyebanji17@marist.com>
2014-09-24 16:29:46 -0400

    Finish Four basic changes

--- AutonomousFunctions.h
+++ AutonomousFunctions.h
@@ -4,6 +4,7 @@
 #pragma config(Motor,  port10,          backRight,     tmotorVex393, openLoop, reversed)
 #include "constants.h"
 
+//linear:  straight  forward, backward in any direction
 void linearMove(int power){
 	LEFT_BACK =	power;
 	RIGHT_BACK = power;
@@ -13,22 +14,70 @@ void linearMove(int power){
 
 void turnInPlace(const string direction, int power){
 	if(direction == "right"){
-		power = -power;
+		LEFT_FRONT = -power;
+		LEFT_BACK = power;
+		RIGHT_FRONT = power;
+		RIGHT_BACK = -power;
 	}
+
+	if (direction ==  "left") {
 	LEFT_BACK =	-power;
-	RIGHT_BACK = -power;
+	RIGHT_BACK = power;
 	LEFT_FRONT = power;
-	RIGHT_FRONT = power;
+	RIGHT_FRONT = -power;
+}
+
+}
+
+//forwardTime
+void forwardTime(int power, int mSecs) {
+
+linearMove(power);
+wait1Msec(mSecs);
+linearMove(0);
+
+}
+//backwardTime
+void backwardTime(int power, int mSecs) {
+linearMove(power);
+wait1Msec(mSecs);
+linearMove(0);
+
+}
+//leftMoveTime: robot goes horizontally left
+void leftMoveTime(int power, int mSecs) {
+	linearMove(power);
+	wait1Msec(mSecs);
+	linerMove(0);
+
+}
+
+//rightMoveTime
+void rightMoveTime(int power, int mSecs) {
+  linearMove(power);
+  wait1Msec(mSecs);
+  linearMove(0);
 }
 
 void turnForSeconds(const string direction, float seconds, int power){
 
 	if(direction == "right") {
 
-		Left_B
+
+		LEFT_BACK = -power;
+		LEFT_FRONT = -power;
+		RIGHT_FRONT = power;
+		RIGHT_BACK = power;
 
 
   }
+
+  if(direction == "left") {
+
+  	LEFT_BACK = power;
+		LEFT_FRONT = power;
+		RIGHT_FRONT = -power;
+		RIGHT_BACK = -power;
 
 }
 
